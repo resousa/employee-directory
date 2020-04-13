f463bb81b9b94da37ce95aec95c7b32a198b2e7f
Ryan Sousa <Sousa.Ryan4@gmail.com>
2020-04-12 22:10:00 -0700

    api working and displaying info

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,119 +1,29 @@
 import React from 'react';
 
-function Table() {
+function Table(props) {
     return (
-      <div className='row py-3'>
-      <div className='col-md-6'>
-        <div className='card'>
-          <div className='card-header font-weight-bold'>Search</div>
-          <div className='card-body'>
-            <form className='form-inline pb-3'>
-              <input
-                className='form-control mr-sm-2'
-                type='search'
-                placeholder='Search'
-                aria-label='Search'
-              ></input>
-            </form>
-
-            <a href='#' className='btn btn-primary'>
-              Search
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className='col-md-6'>
-        <div className='card'>
-          <div className='card-header font-weight-bold'>Add Vendor</div>
-          <div className='card-body'>
-            <div className='input-group mb-3'>
-              <div className='input-group-prepend'>
-                <span
-                  className='input-group-text'
-                  id='inputGroup-sizing-default'
-                >
-                  Name
-                </span>
-              </div>
-              <input
-                type='text'
-                className='form-control'
-                aria-label='Default'
-                aria-describedby='inputGroup-sizing-default'
-              ></input>
-            </div>
-            <div className='input-group mb-3'>
-              <div className='input-group-prepend'>
-                <span
-                  className='input-group-text'
-                  id='inputGroup-sizing-default'
-                >
-                  Contact
-                </span>
-              </div>
-              <input
-                type='text'
-                className='form-control'
-                aria-label='Default'
-                aria-describedby='inputGroup-sizing-default'
-              ></input>
-            </div>
-            <div className='input-group mb-3'>
-              <div className='input-group-prepend'>
-                <span
-                  className='input-group-text'
-                  id='inputGroup-sizing-default'
-                >
-                  Address
-                </span>
-              </div>
-              <input
-                type='text'
-                className='form-control'
-                aria-label='Default'
-                aria-describedby='inputGroup-sizing-default'
-              ></input>
-            </div>
-            <div className='input-group mb-3'>
-              <div className='input-group-prepend'>
-                <span
-                  className='input-group-text'
-                  id='inputGroup-sizing-default'
-                >
-                  Inventory Purchased
-                </span>
-              </div>
-              <input
-                type='text'
-                className='form-control'
-                aria-label='Default'
-                aria-describedby='inputGroup-sizing-default'
-              ></input>
-            </div>
-            <div className='input-group mb-3'>
-              <div className='input-group-prepend'>
-                <span
-                  className='input-group-text'
-                  id='inputGroup-sizing-default'
-                >
-                  Contract End
-                </span>
-              </div>
-              <input
-                type='text'
-                className='form-control'
-                aria-label='Default'
-                aria-describedby='inputGroup-sizing-default'
-              ></input>
-            </div>
-
-            <a href='#' className='btn btn-primary'>
-              Add
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
+      <table className="table table-hover table-dark">
+        <thead>
+          <tr>
+            <th>Photo</th>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Email</th>
+            <th>Birth</th>
+          </tr>
+        </thead>
+          <tbody>
+              {props.employees.map(employee => (
+                <tr>
+                  <td><img alt={`img`} src={employee.picture.medium}></img></td>
+                  <td>{employee.name.first} {employee.name.last}</td>
+                  <td>{employee.phone}</td>
+                  <td>{employee.email}</td>
+                  <td>{new Date(employee.dob.date).toLocaleDateString()}</td>
+                </tr>
+              ))}
+            </tbody>
+            </table>
       );
 }
 
