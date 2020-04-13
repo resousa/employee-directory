79d0464bdbd1ee98304b283da5fd13370990b915
Ryan Sousa <Sousa.Ryan4@gmail.com>
2020-04-13 00:09:09 -0700

    ubdate api result #

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
   getEmployees: function() {
-    return axios.get("https://randomuser.me/api/?results=125&nat=us");
+    return axios.get("https://randomuser.me/api/?results=10&nat=us");
   }
 };
