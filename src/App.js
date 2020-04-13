942d555752bccc14075d621b8e2e857c0f7804c5
Ryan Sousa <Sousa.Ryan4@gmail.com>
2020-04-13 01:29:29 -0700

    deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename={`${process.env.PUBLIC_URL}/`}>
     <div className='App'>
       <Wrapper>
         <Header />
